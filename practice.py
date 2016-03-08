bd4e295af99f019f515ec8380c78e45993d369ae
tsengdodo <annie3530tw@gmail.com>
2016-03-08 19:02:47 +0800

    trying

---
+++ practice.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+my_age=18
+month=9
+day=20
+a=5
+b=4
+c=-5
+d=1.2345
+print my_age 
+print u"我得生日是%d月%d號"%(month,day)
+print "a*b=",a*b
+print "a/b=",a/b
+print a-b
+print a+b
+print a>>b
+print a<<b
+print abs(c)
+print divmod(a,b)
+print round(d,2)
+print pow(a,b,3)
